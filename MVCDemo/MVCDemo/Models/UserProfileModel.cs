5b20295ea3552739a1cee3fc4aaa8732ec740660
Karen Spriggs <61596604+karenspriggs@users.noreply.github.com>
2023-01-26 18:34:34 -0600

    adding first mvc demo

---
+++ MVCDemo/MVCDemo/Models/UserProfileModel.cs
@@ -0,0 +1,28 @@
+﻿namespace MVCDemo.Models
+{
+    public class UserProfileModel
+    {
+        public string FirstName;
+        public string LastName;
+        public int Age;
+        public string Occupation;
+
+        public string Description
+        {
+            get
+            {
+                return $"{FirstName}/{LastName}, {Age}. {Occupation}.";
+            }
+        }
+
+        public string EstimatedBirthYear
+        {
+            get
+            {
+                int currentYear = DateTime.Now.Year;
+                return $"If already had your birthday this year, then you were born in {currentYear-Age}, " +
+                    $"otherwise you were born in {currentYear-Age-1}";
+            }
+        }
+    }
+}
