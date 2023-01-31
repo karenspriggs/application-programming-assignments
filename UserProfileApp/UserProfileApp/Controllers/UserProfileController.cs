4d525ea1382f015b179c0702f3fbe34e4b36c4e4
Karen Spriggs <k.spriggs01@gmail.com>
2023-01-31 14:40:48 -0600

    Adding documentation

--- UserProfileApp/UserProfileApp/Controllers/UserProfileController.cs
+++ UserProfileApp/UserProfileApp/Controllers/UserProfileController.cs
@@ -14,16 +14,19 @@ namespace UserProfileApp.Controllers
             return View(new Models.UserProfile());
         }
 
+        // This action is executed when the user clicks the submit button
+        // If they fill out the form again and hit submit, the old values are replaced with the new ones
         // POST: UserProfile/Create
         [HttpPost]
         public ActionResult Index(Models.UserProfile userProfile)
         {
             int userAge = userProfile.Age;
 
-            //It thinks the year is 2024 when I test this, hence the -1
+            // System.DateTime.Now.Year is returning the year 2024, hence the -1
             int yearIfHadBDay = System.DateTime.Now.Year -1 -userAge;
             int yearIfNoBDay = yearIfHadBDay++;
 
+            // Using viewbag to store values that populate the html content for the profile section
             ViewBag.Description = $"{userProfile.FirstName}/{userProfile.LastName},{userProfile.Age},{userProfile.Occupation}";
             ViewBag.RandomQuote = MovieQuoteDatabase.GetRandomQuote();
             ViewBag.EstimatedBirthYear = $"If you had your birthday this year, I think you were born in {yearIfHadBDay}, otherwise you were born in {yearIfNoBDay}";
