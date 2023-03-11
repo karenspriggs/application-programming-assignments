603447b5ab84134aa55e36d2307a55a0f5f1caff
Karen Spriggs <k.spriggs01@gmail.com>
2023-03-11 14:46:02 -0600

    making test project

--- UnitTestDemo/UnitTestDemo/Properties/AssemblyInfo.cs
+++ UnitTestDemo/UnitTestDemo/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AuthenticationExample")]
+[assembly: AssemblyTitle("UnitTestDemo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AuthenticationExample")]
+[assembly: AssemblyProduct("UnitTestDemo")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d0daff3d-e32a-41a9-b1fc-1d21175fddb9")]
+[assembly: Guid("2f371a23-c4c3-466c-b360-8438f26674fa")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestDemo/UnitTestDemo/Utilities.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace UnitTestDemo
+{
+    public class Utilities
+    {
+        public static double GetFloorForDouble(double input)
+        {
+            return Math.Floor(input);
+        }
+
+        public static string NameJoiner(string firstname, string lastname)
+        {
+            return $"{firstname}, {lastname}";
+        }
+
+        public static string StringHasher(string input)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    stringBuilder.Append(bytes[i].ToString("x2"));
+                }
+
+                return stringBuilder.ToString();
+            }
+        }
+
+        public static bool IsPrime(int input)
+        {
+            if (input % 2 == 0) return false;
+
+            for(int i = 0; i < input/2; i++)
+            {
+                if (input % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static List<int> GetAllNonPrimeDivisors(int input)
+        {
+            List<int> divisors = new List<int>();
+
+            for(int i = 0; i < input / 2; i++)
+            {
+                if (!IsPrime(i) && input % i == 0)
+                {
+                    divisors.Add(i);
+                }
+            }
+
+            return divisors;
+        }
+
+        public static double GetFloorOfFraction(double num1, double num2)
+        {
+            double numerator;
+            double denominator;
+
+            if (num1 > num2)
+            {
+                numerator = num1;
+                denominator = num2;
+            } else
+            {
+                numerator = num2;
+                denominator = num1;
+            }
+
+            return GetFloorForDouble(numerator / denominator);
+        }
+
+        public static Dictionary<int, int> MakeOddDictionary(int input)
+        {
+            Dictionary<int, int> oddDictionary = new Dictionary<int, int>();
+            List<int> nums = GetAllNonPrimeDivisors(input);
+            int currentKey = 1;
+
+            foreach(int num in nums)
+            {
+                oddDictionary.Add(currentKey, num);
+                currentKey += 2;
+            }
+
+
+            return oddDictionary;
+        }
+    }
+}
